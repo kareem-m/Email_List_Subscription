34a5358d28f12ed4e6872519c2d71fe5b55b4d1a
Kareem Elramady <kareemmoh1911@gmail.com>
2025-12-03 20:33:50 +0200

    fix some bugs

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ if ($conn->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Add Email
-    if (!empty($_POST['email'])) {
-        $email = $_POST['email'];
+    if (!empty($_POST['add_email'])) {
+        $email = $_POST['add_email'];
 
         $stmt = $conn->prepare("INSERT INTO subscriptions (Email) VALUES (?)");
         $stmt->bind_param("s", $email);
@@ -38,8 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Remove Email
     if (!empty($_POST['remove_email'])) {
         $remove_email = $_POST['remove_email'];
-
-        // Delete email
         $stmt = $conn->prepare("DELETE FROM subscriptions WHERE Email = ?");
         $stmt->bind_param("s", $remove_email);
 
@@ -56,6 +54,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $message = "Error: " . $stmt->error;
         }
 
+        $stmt->close();
+    }
+
+    // Add Employee
+    if (!empty($_POST['add_employee'])) {
+        $addEmpSSN = $_POST['add_employee'];
+
+        $stmt = $conn->prepare("INSERT INTO employees (SSN) VALUES (?)");
+        $stmt->bind_param("s", $addEmpSSN);
+
+        $color = "#73AF6F";
+        if ($stmt->execute()) {
+            $message = "Email added successfully!";
+        } else {
+            $color = "#FF3838";
+            if ($stmt->errno == 1062) {
+                $message = "This email already exists!";
+            } else {
+                $message = "Error: " . $stmt->error;
+            }
+        }
         $stmt->close();
     }
 }
@@ -85,7 +104,7 @@ $conn->close();
         <main>
             <h1>Email List Subscription</h1>
 
-            <div class="buttons">
+            <div class="Mbuttons">
                 <button id="button1">Manage Departments</button>
                 <button id="button2">Manage Employees</button>
                 <button id="button3">Manage Emails</button>
@@ -94,16 +113,48 @@ $conn->close();
             <div id="box1" class="box">
                 <i class="close">x</i>
             </div>
+
             <div id="box2" class="box">
                 <i class="close">x</i>
+
+                <div class="buttons">
+                    <button>Add Employee</button>
+                    <button>Remove Employee</button>
+                    <button>Update Employee</button>
+                </div>
+
+                <!-- <div class="addEmployee">
+                    <p>Add Emploee to Database</p>
+                    <form action="" method="post">
+                        <input type="text" id="add_employee" name="add_employee" placeholder="Enter employee SSN" required>
+                        <input type="submit" value="Add">
+                    </form>
+                </div>
+
+                <div class="removeEmployee">
+                    <p>Remove Employee from Database</p>
+                    <form action="" method="post">
+                        <input type="text" id="remove_employee" name="remove_employee" placeholder="Enter employee SSN" required>
+                        <input type="submit" value="Remove">
+                    </form>
+                </div> -->
+
+                <!-- <div class="updateEmployee">
+                    <p>Update Full Name</p>
+                    <form action="" method="post">
+                        <input type="text" id="updateFName" name="updateFName" placeholder="" required>
+                        <input type="submit" value="Remove">
+                    </form>
+                </div> -->
             </div>
+
             <div id="box3" class="box">
                 <i class="close">x</i>
 
                 <div class="addEmail">
                     <p>Add Email to Database</p>
                     <form action="" method="post">
-                        <input type="email" id="email" name="email" placeholder="Enter email" required>
+                        <input type="email" id="add_email" name="add_email" placeholder="Enter email" required>
                         <input type="submit" value="Add">
                     </form>
                 </div>
@@ -112,7 +163,7 @@ $conn->close();
                     <p>Remove Email from Database</p>
                     <form action="" method="post">
                         <input type="email" id="remove_email" name="remove_email" placeholder="Enter email" required>
-                        <input type="submit" name="remove_submit" value="Remove">
+                        <input type="submit" value="Remove">
                     </form>
                 </div>
 
